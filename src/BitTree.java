c0b70be81fd5fce206b9d4f039d53d5347c22219
MARINA_ANANIAS <ananias@grinnell.edu>
2024-04-23 21:39:06 -0500

    First Set-up mp9

---
+++ src/BitTree.java
@@ -0,0 +1,16 @@
+/**
+ * Class intended to store mappings from bits to values.
+ * @author Marina Ananias
+ */
+
+public class BitTree {
+  // +--------------+------------------------------------------------
+  // | Fields |
+  // +--------+
+
+  // +--------------+------------------------------------------------
+  // | Constructors |
+  // +--------------+
+
+
+}
